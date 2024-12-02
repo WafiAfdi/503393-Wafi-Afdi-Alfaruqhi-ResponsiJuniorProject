b9ba551c9fc7a8fa308a60eabca580ed46a3a8ab
WafiAfdi <wafiafdialfaruqhi@mail.ugm.ac.id>
2024-12-02 08:16:53 +0700

    add : koneksi database

--- Proyek_Responsi/Program.cs
+++ Proyek_Responsi/Program.cs
@@ -1,3 +1,5 @@
+using Proyek_Responsi.ViewModel;
+
 namespace Proyek_Responsi
 {
     internal static class Program
@@ -11,7 +13,10 @@ namespace Proyek_Responsi
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
+            Application.Run(new Form1()
+            {
+                DataContext = new MainWindowViewModel()
+            });
         }
     }
 }
---
+++ Proyek_Responsi/Service/CRUDService.cs
@@ -0,0 +1,29 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyek_Responsi.Service
+{
+    public class CRUDService
+    {
+        private NpgsqlConnection _conn;
+        public string ConnectionString_ = "Host=localhost;Username=postgres;Password=password;Port=5432;Database=responsi";
+
+        public CRUDService()
+        {
+            _conn = new NpgsqlConnection(ConnectionString_);
+            try
+            {
+                _conn.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error Occured : {ex.Message}");
+            }
+        }
+
+    }
+}
